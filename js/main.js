1a9b57c4e03b82e752eb7c9c6c97b868751b16d5
Eunbi Hong <hong00412@naver.com>
2022-07-04 09:33:10 +0900

    section1 css

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// (() => {
+//     function arrowD() {
+//         const arrow = document.querySelector('.homeArrow');
+//         const arrowBtn = document.querySelector('.arrowBtn');
+//         arrowBtn.addEventListener('mouseover', () => {
+//             arrow.setAttribute('d', 'M1.6,9.4l25.7,0c0,0,0.1,0,0-0.1l-6.9-6.7c-0.7-0.6-0.7-1.6,0-2.2c0.7-0.6,1.7-0.6,2.3,0l9.8,9.4c0.3,0.3,0.5,0.7,0.5,1.1s-0.2,0.9-0.5,1.1l-9.7,9.3c-0.7,0.6-1.8,0.8-2.4,0.1c-0.7-0.6-0.7-1.6,0-2.2l7-6.6c0,0,0-0.1,0-0.1l-25.7,0C0.7,12.6,0,11.9,0,11C0,10.1,0.7,9.4,1.6,9.4z')
+//             console.log(arrow.getAttribute('d'));
+//         });
+//         arrowBtn.addEventListener('mouseout', () => {
+//             arrow.setAttribute('d', 'M6.1,9.4l17.2,0c0,0,0.1,0,0-0.1l-6.9-6.7c-0.7-0.6-0.7-1.6,0-2.2c0.7-0.6,1.7-0.6,2.3,0l9.8,9.4c0.3,0.3,0.5,0.7,0.5,1.1s-0.2,0.9-0.5,1.1l-9.7,9.3c-0.7,0.6-1.8,0.8-2.4,0.1c-0.7-0.6-0.7-1.6,0-2.2l7-6.6c0,0,0-0.1,0-0.1l-17.2,0c-0.9,0-1.6-0.7-1.6-1.6C4.5,10.1,5.2,9.4,6.1,9.4z')
+//             console.log(arrow.getAttribute('d'));
+//         })
+        
+//         console.log(arrowBtn);
+//     }
+//     arrowD();
+// })();
